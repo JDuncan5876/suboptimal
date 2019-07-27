3db7aac78ff0a9502a358759305f3820dac8ca0d
Advait-M <advait.maybhate9@gmail.com>
2019-07-26 23:14:25 -0400

    Strip text of all non-letter characters including spaces and normalize to lowercase.

--- analyze_picture.py
+++ analyze_picture.py
@@ -1,10 +1,14 @@
 import io
 import os
+import string
 
 # Imports the Google Cloud client library
 from google.cloud import vision
 from google.cloud.vision import types
 from google.oauth2 import service_account
+
+def strip_text(text):
+	return "".join([i for i in text.replace("\n", "").lower() if i in string.ascii_lowercase])
 
 # Load credentials
 credentials = service_account.Credentials. from_service_account_file('suboptimal-dbf800ff4b45.json')
@@ -25,6 +29,4 @@ image = types.Image(content=content)
 response = client.text_detection(image=image)
 texts = response.text_annotations
 
-print('Texts:')
-for text in texts:
-    print('\n"{}"'.format(text.description))
+print(strip_text(texts[0].description))
