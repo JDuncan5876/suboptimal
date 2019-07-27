cf6191e7ce4d9fc074c816227846c76a27d3d8e4
Jared Duncan <JDuncan5876@gmail.com>
2019-07-27 12:23:50 -0400

    Remove key, read regenerated key from file

--- get_route.py
+++ get_route.py
@@ -2,8 +2,11 @@ import googlemaps
 from datetime import datetime
 import json
 
+with open("key.txt", "r") as keyfile:
+    key = keyfile.read()
+
 def get_route(start, end):
-    gmaps = googlemaps.Client(key="AIzaSyDO1zQVX5ZNS3yvtJt0M0fHqA_0x2tD2Wc")
+    gmaps = googlemaps.Client(key=key)
     directions_result = gmaps.directions(start, end, mode="transit", departure_time=datetime.now(), transit_mode="subway")
     results = [(step["transit_details"]["departure_stop"]["name"], step["transit_details"]["arrival_stop"]["name"], step["transit_details"]["num_stops"]) 
         for step in directions_result[0]["legs"][0]["steps"] if step["travel_mode"] == "TRANSIT"]
