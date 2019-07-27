a12bafa374f242ea22ebbe3b9b46dcaf90f7a50d
Jared Duncan <JDuncan5876@gmail.com>
2019-07-26 23:31:38 -0400

    Modified analyze picture to function-usable form

--- analyze_picture.py
+++ analyze_picture.py
@@ -8,25 +8,28 @@ from google.cloud.vision import types
 from google.oauth2 import service_account
 
 def strip_text(text):
-	return "".join([i for i in text.replace("\n", "").lower() if i in string.ascii_lowercase])
+	return "".join([i for i in text.replace("\n", "").lower() if i in string.ascii_lowercase or i in string.digits])
 
-# Load credentials
-credentials = service_account.Credentials. from_service_account_file('suboptimal-dbf800ff4b45.json')
+def analyze_image(file_name):
+    # Load credentials
+    credentials = service_account.Credentials. from_service_account_file('/home/pi/suboptimal-dbf800ff4b45.json')
 
-# Instantiates a client
-client = vision.ImageAnnotatorClient(credentials=credentials)
+    # Instantiates a client
+    client = vision.ImageAnnotatorClient(credentials=credentials)
 
-file_name = os.path.join(
-    os.path.dirname(__file__),
-    'image6.jpg')
+    # Loads the image into memory
+    with io.open(file_name, 'rb') as image_file:
+        content = image_file.read()
 
-# Loads the image into memory
-with io.open(file_name, 'rb') as image_file:
-    content = image_file.read()
+    image = types.Image(content=content)
 
-image = types.Image(content=content)
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    return strip_text(texts[0].description)
 
-response = client.text_detection(image=image)
-texts = response.text_annotations
+if __name__ == "__main__":
+    file_name = os.path.join(
+        os.path.dirname(__file__),
+        '/home/pi/42st.jpg')
+    print(analyze_image(file_name))
 
-print(strip_text(texts[0].description))
